319bc382e5d7e91c9da53d8fe03f02b13ffb90e8
Amena16 <coulibaly.amena@courrier.uqam.ca>
2025-09-16 05:32:29 +0200

    Mon devoir est ici

--- devoir1/getidx.R
+++ devoir1/getidx.R
@@ -1,3 +1,14 @@
 getidx <- function(M, i, j) {
-    ## Votre code ici!
+    n<-nrow(M)
+    index<-(j-1)* n+ i
+    return((as.vector(M)[[index]]))
 }
+
+M<-matrix(1:9,nrow=3,ncol=3)
+print(M)
+getidx(M,2,3)
+
+
+M1<-matrix(1:16,nrow = 4,ncol=4)
+print(M1)
+getidx(M1,3,4)
