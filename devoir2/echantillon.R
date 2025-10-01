627e9326fab12e1050f10f383f7833f9b35439cf
Amena16 <coulibaly.amena@courrier.uqam.ca>
2025-10-02 01:53:26 +0200

    mon code pour l'exercice 2 est ici

---
+++ devoir2/echantillon.R
@@ -0,0 +1,20 @@
+## (a)
+echantillon <- function(..., centrer = FALSE, reduire = FALSE) {
+    ## Votre code ici
+}
+
+## (b)
+
+print.echantillon <- function(x) {
+    ## Votre code ici
+}
+
+## Implémentez la méthode `summary` ici
+
+## (c)
+
+# Implémentez la méthode `lm` ici
+
+## (d)
+
+# Implémentez vos fonctions ici
